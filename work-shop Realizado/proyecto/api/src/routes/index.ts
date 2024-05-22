e5787bacffda7e2d76706f74577c6fac1442df4f
ArielZarate <arieltecnico@gmail.com>
2024-05-22 10:58:17 -0300

    borre el node_modules del api

--- work-shop Realizado/proyecto/api/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express ,{Response,request,Router}from 'express';
-import user from './user';
-const router:Router=express.Router();
-
-router.use('/user',user);
-
-
-
-export default router;
--- work-shop Realizado/wks-typescript/src/interface/AppProps.tsx
+++
@@ -1,6 +0,0 @@
-
-
-
-export interface AppProps {
-    title: string;
-}
