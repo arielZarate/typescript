70dfea977efb2b3910fe5aeeabcfe1a1643fc483
ArielZarate <arieltecnico@gmail.com>
2024-05-23 18:15:57 -0300

    add project with typescript v3 deploy carddetail

--- typescript-naruto/src/app/shippuden/[id]/page.tsx
+++ typescript-naruto/src/app/shippuden/[id]/page.tsx
@@ -26,6 +26,75 @@ const NinjaDetail = () => {
   }
 
   return (
+    <div className="my-20 py-10 px-2 mx-2 text-white hover:cursor-pointer">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 border-4 border-slate-500 rounded-md">
+        <div className="md:border-r-4 border-slate-200">
+          <h1 className="font-bold text-3xl text-center py-3 text-slate-300 rounded-sm hover:text-green-500 duration-300">
+            {ninja.nombre}
+          </h1>
+          <div className="min-w-full h-full ">
+            <img
+              src={ninja.avatar}
+              alt={ninja.nombre}
+              className="object-cover w-full h-[470px] rounded-sm duration-200"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col py-5 gap-2 px-4">
+          <div className="flex flex-wrap justify-between ">
+            <p className="text-sm">
+              <strong className="mr-1">Aldea:</strong>
+              <span className="hover:text-orange-500"> {ninja.aldea}</span>
+            </p>
+            <p className="mr-2 text-sm">
+              <strong>Clan:</strong>
+              <span className="hover:text-orange-500"> {ninja.clan}</span>
+            </p>
+          </div>
+          <p className="text-sm">
+            <strong className="text-green-400 mr-1">Estado:</strong>
+            <span className="hover:text-green-500"> {ninja.estado}</span>
+          </p>
+          <p className="text-sm">
+            <strong className="mr-1">Edad:</strong>
+            <span className="hover:text-orange-500"> {ninja.edad}</span>
+          </p>
+          <ul className="border-2 border-slate-400 w-52 rounded-md p-2">
+            <strong className="mr-1 p-1 text-blue-700">Habilidades:</strong>
+            <div>
+              {ninja.habilidades.map((e) => (
+                <li
+                  key={e}
+                  className="text-sm font-light hover:text-orange-500"
+                >
+                  {e}
+                </li>
+              ))}
+            </div>
+          </ul>
+          <div>
+            <strong className="text-blue-700">Descripcion</strong>
+            <div className="text-sm font-light flex flex-wrap">
+              {ninja.historia.split(" ").map((word, index) => (
+                <span key={index} className="hover:text-orange-500 mr-1">
+                  {word}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NinjaDetail;
+
+/*
+
+
+
+
     <div className=" my-20 py-10 ml-10 text-white  hover:cursor-pointer">
       <div className="grid grid-cols-2 gap-1 border-4 border-slate-500 h-[full] w-4/6 rounded-md">
         <div className="">
@@ -92,7 +161,5 @@ const NinjaDetail = () => {
         </div>
       </div>
     </div>
-  );
-};
 
-export default NinjaDetail;
+*/
